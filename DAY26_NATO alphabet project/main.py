278035d38d60ae1e839dd645b57cff4c661b3777
CSroseX <115336076+CSroseX@users.noreply.github.com>
2023-07-04 23:46:22 +0530

    Update main.py

--- DAY26_NATO alphabet project/main.py
+++ DAY26_NATO alphabet project/main.py
@@ -1,8 +1,8 @@
 import pandas
 
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
-dict  = {row.letter: row.code for (index,row) in data.iterrows()}
+dict  = {row.letter: row.code for (index,row) in data.iterrows()} #dictionary comprehension
  
 word = input('Enter a word : ').upper()
-output_list = [dict[letter] for letter in word]
+output_list = [dict[letter] for letter in word] #list comprehension
 print (output_list)
