061bb522b39e5ef044ee5e8a0021195b3c1b5971
CSroseX <115336076+CSroseX@users.noreply.github.com>
2023-07-13 17:53:59 +0530

    Update main.py

--- DAY29_passord manager/main.py
+++ DAY29_passord manager/main.py
@@ -1,7 +1,11 @@
+# Add a functionality such that when an already saved pss is present, ask the user if they want to overwrite it
+# also make messagebox which shows all the list of websites saved 
+
 from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
@@ -31,6 +35,22 @@ def generate_password():
     busted_display = Label(window, text="*Password copied !", font=("arial", "9"))
     busted_display.grid(column=0,row=5)
     window.after(2000, busted_display.destroy)
+# ------------------------------- SEARCH ----------------------------------- #
+def search():
+    global website_ENTRY
+    website_search = website_ENTRY.get()
+    try:
+        with open('data.json') as data_search:
+            data = json.load(data_search)
+    except FileNotFoundError:
+        messagebox.showinfo(title='ERROR',message='No data file found !')
+    else:
+        if website_search in data:
+            usr = data[website_search]['email']
+            pss = data[website_search]['password']
+            messagebox.showinfo(title=website_search, message=f'Email : {usr}\npassword : {pss}')
+        else:
+            messagebox.showinfo(title='Error',message=f'No details for {website_search} was found')
     
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -39,19 +59,29 @@ def save():
     website_data = website_ENTRY.get()
     usr_data = usr_ENTRY.get()
     pss_data = pss_ENTRY.get()
+    new_data = {
+        website_data :{
+        'email' : usr_data,
+        'password': pss_data,}
+    }
 
 
     if len(website_data)==0 or len(usr_data)==0 or len(pss_data)==0:
         messagebox.askokcancel(title='Field(s) empty',message='You have left one or more fields empty')
-    
     else:
-        is_ok = messagebox.askokcancel(title='Save?',message=f'Here are your details\n  Website: {website_data}\n  email/username: {usr_data}\n  password: {pss_data}\nWanna confirm save ?')
-        if is_ok :
-            #save to data.txt
-            data_file = open('data.txt','a')
-            data_file.write(f'{website_data} | {usr_data} | {pss_data} \n')
-            data_file.close()
-
+        try:
+            with open('data.json','r') as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open('data.json','w') as data_file:
+                json.dump(new_data,data_file,indent=4)
+        else:
+            data.update(new_data)
+            with open('data.json','w') as data_file:
+                #Saving the updated data
+                json.dump(data, data_file,indent=4)
+        
+        finally:
             #remove existing contents from the UI
             website_ENTRY.delete(0,'end')
             pss_ENTRY.delete(0,'end')
@@ -69,9 +99,9 @@ canvas.grid(column=1,row=0)
 website_LABEL = Label(text='Website:')
 website_LABEL.grid(column=0,row=1)
 
-website_ENTRY = Entry(width=39)
+website_ENTRY = Entry(width=21)
 website_ENTRY.focus()
-website_ENTRY.grid(column=1,row=1,columnspan=2)
+website_ENTRY.grid(column=1,row=1)
 
 usr_LABEL = Label(text='Email/Username: ')
 usr_LABEL.grid(column=0,row=2)
@@ -91,5 +121,8 @@ generate_BUTTON.grid(column=2,row=3)
 
 add_BUTTON = Button(text='Add',width=36,command=save)
 add_BUTTON.grid(column=1,row=4,columnspan=2)
+
+search_BUTTON = Button(text='Search',command=search,width=15)
+search_BUTTON.grid(column=2,row=1)
 
 window.mainloop()
